5f4458c282a5f6e9d7ca6f836000bbf5dae1684c
rchermanteev <rchermanteev@bk.ru>
2020-10-02 12:33:23 +0300

    updated solution to the problem of finding the determinant

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -2,68 +2,68 @@
 
 .global count_det
 count_det:
-	push {r4-r12}
+	push {r4-r10}
 
-	ldrb r4, [r0], #4
-	ldrb r5, [r0], #4
-	ldrb r6, [r0], #4
-	ldrb r7, [r0], #4
-	ldrb r8, [r0], #4
-	ldrb r9, [r0], #4
-	ldrb r10, [r0], #4
-	ldrb r11, [r0], #4
-	ldrb r12, [r0], #4
+	ldr r2, [r0], #4
+	ldr r3, [r0], #4
+	ldr r4, [r0], #4
+	ldr r5, [r0], #4
+	ldr r6, [r0], #4
+	ldr r7, [r0], #4
+	ldr r8, [r0], #4
+	ldr r9, [r0], #4
+	ldr r10, [r0], #4
 
 	mov r0, #0
 	mov r1, #1
 	
-	mul r1, r4
-	mul r1, r8
-	mul r1, r12
+	mul r1, r2
+	mul r1, r6
+	mul r1, r10
 
 	add r0, r1
 
 	mov r1, #1
 
+	mul r1, r3
+	mul r1, r7
+	mul r1, r8
+
+	add r0, r1
+
+	mov r1, #1 
+
 	mul r1, r5
 	mul r1, r9
-	mul r1, r10
-
-	add r0, r1
-
-	mov r1, #1 
-	
-	mul r1, r6
-	mul r1, r7
-	mul r1, r11
+	mul r1, r4
 
 	add r0, r1
 
 	mov r1, #1 
 
+	mul r1, r4
 	mul r1, r6
 	mul r1, r8
-	mul r1, r10
 
 	sub r0, r1
 
 	mov r1, #1
 
-	mul r1, r4
 	mul r1, r9
-	mul r1, r11
+	mul r1, r7
+	mul r1, r2
 
 	sub r0, r1
 
 	mov r1, #1 
 
 	mul r1, r5
-	mul r1, r7
-	mul r1, r12
+	mul r1, r3
+	mul r1, r10
 
 	sub r0, r1
-	
-	pop {r4-r12}
+
+	pop {r4-r10}
 
 	bx lr
 
