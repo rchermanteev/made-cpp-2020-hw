f3368c056731eb85d3595285c447163726458ed7
rchermanteev <rchermanteev@bk.ru>
2020-11-10 23:41:57 +0300

    added solution for the third project

--- list/src/list.h
+++ list/src/list.h
@@ -1,70 +1,197 @@
 #pragma once
 #include <iterator>
+#include <iostream>
+#include <string>
 
 
 namespace task {
 
+template<class T>
+class Node {
+public:
+	Node() {};
+	Node(T value): val(value) {};
+	Node(T&& value, Node<T>* next, Node<T>* prev): val(std::move(value)), next(next), prev(prev) {};
+	Node(const Node<T>& other): val(other.val), next(other.next), prev(other.prev) {};
+	~Node() {};
+
+	Node<T>* next;
+	Node<T>* prev;
+	T val;
+};
+
 
 template<class T, class Alloc = std::allocator<T>>
 class list {
+private:
+    typedef std::allocator_traits<Alloc> _traits;
+
+public:
+    typedef T value_type;
+    typedef Alloc allocator_type;
+    typedef typename _traits::size_type size_type;
+    typedef typename _traits::difference_type difference_type;
+    typedef typename _traits::pointer pointer;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+    typedef typename _traits::const_pointer const_pointer;    
+    using node_type = Node<T>;
+    using node_pointer = Node<T>*;
 
 public:
     class iterator {
-    public:
+    public: 
+    	friend class list;
         using difference_type = ptrdiff_t;
         using value_type = T;
         using pointer = T*;
         using reference = T&;
         using iterator_category = std::bidirectional_iterator_tag;
-
-        iterator();
-        iterator(const iterator&);
-        iterator& operator=(const iterator&);
-
-        iterator& operator++();
-        iterator operator++(int);
-        reference operator*() const;
-        pointer operator->() const;
-        iterator& operator--();
-        iterator operator--(int);
-
-        bool operator==(iterator other) const;
-        bool operator!=(iterator other) const;
-
-        // Your code goes here?..
+        using node_type = Node<T>;
+        using node_pointer = Node<T>*;
 
     private:
-        // Your code hoes here...
+        node_pointer node;    
+        
+    public:    
+
+        iterator(): node(0) {};
+
+        iterator(node_pointer node): node(node) {};
+
+        iterator(const iterator& it): node(it.node) {};
+
+        iterator& operator=(const iterator& it) { node = it.node; return *this; };
+        iterator& operator++() { node = node->next; return *this; }
+        iterator operator++(int) { iterator t(*this); t = t->next; return t; };
+        reference operator*() const { return node->val; };
+		pointer operator->() const { return &(node->val); };
+        iterator& operator--() { node = node.prev; return *this; };
+        iterator operator--(int) { iterator t(*this); t = t.prev; return t; };
+        bool operator==(iterator other) const { return (node == other.node); };
+		bool operator!=(iterator other) const { return !(*this == other); };
     };
 
     class const_iterator {
-        // Your code goes here...
+    public: 
+    	friend class list;
+        using difference_type = ptrdiff_t;
+        using value_type = T;
+        using const_pointer = const T*;
+        using const_reference = const T&;
+        using iterator_category = std::bidirectional_iterator_tag;
+        using const_node_type = const Node<T>;
+        using const_node_pointer = const Node<T>*;
+
+    private:
+        const_node_pointer node;    
+        
+    public:    
+        const_iterator(): node(0) {};
+        const_iterator(const_node_pointer node): node(node) {};
+        const_iterator(const_iterator& it): node(it.node) {};
+
+        const_iterator& operator=(const_iterator& it) { node = it.node; return *this; };
+        const_iterator& operator++() { node = node->next; return *this; }
+        const_iterator operator++(int) { const_iterator t(*this); t = t->next; return t; };
+        const_reference operator*() const { return node->val; };
+		const_pointer operator->() const { return &(node->val); };
+        const_iterator& operator--() { node = node.prev; return *this; };
+        const_iterator operator--(int) { const_iterator t(*this); t = t.prev; return t; };
+        bool operator==(const_iterator other) const { return (node == other.node); };
+		bool operator!=(const_iterator other) const { return !(*this == other); };
     };
 
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
+    static_assert(
+        std::is_same<typename Alloc::value_type, value_type>::value,
+        "Allocator::value_type must be same type as value_type"
+    );
 
-    list();
-    explicit list(const Alloc& alloc);
+private:
+	allocator_type _allocator;
+
+	std::allocator<node_type> allocator_for_node;
+
+    node_pointer _head;
+    node_pointer _tail;
+
+    size_t _size = 0;
+
+    iterator head_iterator;
+    iterator tail_iterator;
+
+
+public:
+
+    list() {
+    	_head = _tail = allocator_for_node.allocate(1);
+    	_tail = nullptr;
+    	_head = nullptr;
+
+    	head_iterator = iterator(_head);
+    	tail_iterator = iterator(_tail);
+    };
+    
+    explicit list(const Alloc& alloc) {};
+    
     list(size_t count, const T& value, const Alloc& alloc = Alloc());
-    explicit list(size_t count, const Alloc& alloc = Alloc());
+    
+    list(size_t count, const Alloc& alloc = Alloc()) {
+    	while (this->size() < count)
+    		this->push_back();
+    }
 
-    ~list();
+    ~list() {
+    	if (_size){
+    		node_pointer tmp;
+	    	while(_head) {
+		        tmp = _head;
+		        _head = _head->next;
+		        allocator_for_node.deallocate(tmp, 1);
+	    	}
+    	}
+    };
 
     list(const list& other);
+
     list(list&& other);
+
     list& operator=(const list& other);
     list& operator=(list&& other);
 
     Alloc get_allocator() const;
 
 
-    T& front();
-    const T& front() const;
+    T& front() {
+    	if(_head == nullptr)
+	        throw std::runtime_error("Invalid Action!");
 
-    T& back();
-    const T& back() const;
+	    return _head->val;
+    };
+
+    const T& front() const {
+    	if(_head == nullptr)
+	        throw std::runtime_error("Invalid Action!");
+
+	    return _head->val;
+    }
+
+    T& back() {
+    	if(_tail == nullptr)
+	        throw std::runtime_error("Invalid Action!");
+
+	    return _tail->val;
+    };
+
+    const T& back() const {
+    	if(_tail == nullptr)
+	        throw std::runtime_error("Invalid Action!");
+
+	    return _tail->val;
+    };
 
 
     iterator begin();
@@ -80,10 +207,23 @@ public:
     const_reverse_iterator crend() const;
 
 
-    bool empty() const;
-    size_t size() const;
-    size_t max_size() const;
-    void clear();
+    bool empty() const { return (_size == 0); };
+
+    size_t size() const { return _size; };
+    size_t max_size() const { return _size; };
+
+    void clear() {
+    	if (_size){
+    		node_pointer tmp;
+	    	while(_head) {
+		        tmp = _head;
+		        _head = _head->next;
+		        allocator_for_node.deallocate(tmp, 1);
+	    	}
+    	}
+    	_head = _tail = nullptr;
+    	_size = 0;
+    };
 
     iterator insert(const_iterator pos, const T& value);
     iterator insert(const_iterator pos, T&& value);
@@ -93,13 +233,94 @@ public:
     iterator erase(const_iterator first, const_iterator last);
 
 
-    void push_back(const T& value);
-    void push_back(T&& value);
-    void pop_back();
+    void push_back(const T& value) {
+		node_pointer new_node = allocator_for_node.allocate(1);
+		new_node->next = nullptr;
+		new_node->prev = _tail;
+		new_node->val = value;
 
-    void push_front(const T& value);
-    void push_front(T&& value);
-    void pop_front();
+		if(_head == nullptr)
+		    _head = new_node;
+
+		if(_tail != nullptr)
+		    _tail->next = new_node;
+
+		_tail = new_node;
+		++ _size;
+    };
+
+    void push_back(T&& value = T()) {
+		node_pointer new_node = allocator_for_node.allocate(1);
+		allocator_for_node.construct(new_node, std::move(value), nullptr, _tail);
+
+		if(_head == nullptr)
+		    _head = new_node;
+
+		if(_tail != nullptr)
+		    _tail->next = new_node;
+
+		_tail = new_node;
+		++ _size;
+    };
+
+    void pop_back() {
+    	if(_tail == nullptr)
+	        throw std::runtime_error("Invalid Action");
+
+	    node_pointer tmp = _tail;
+
+	    _tail = _tail->prev;
+	    if(_tail != nullptr)
+	        _tail->next = nullptr;
+
+	    -- _size;
+
+	    allocator_for_node.deallocate(tmp, 1);
+    };
+
+    void push_front(const T& value) {
+		node_pointer new_node = allocator_for_node.allocate(1);
+		new_node->next = _head;
+		new_node->prev = nullptr;
+		new_node->val = value;
+
+		if(_tail == nullptr)
+		    _tail = new_node;
+
+		if(_head != nullptr)
+		    _head->prev = new_node;
+
+		_head = new_node;
+		++ _size;
+    };
+
+    void push_front(T&& value = T()) {
+    	node_pointer new_node = allocator_for_node.allocate(1);
+    	allocator_for_node.construct(new_node, std::move(value), _head, nullptr);
+
+		if(_tail == nullptr)
+		    _tail = new_node;
+
+		if(_head != nullptr)
+		    _head->prev = new_node;
+
+		_head = new_node;
+		++ _size;
+    }
+
+    void pop_front() {
+    	if(_head == nullptr)
+	        throw std::runtime_error("Invalid Action");
+
+	    node_pointer tmp = _head;
+
+	    _head = _head->next;
+	    if(_head != nullptr)
+	        _head->prev = nullptr;
+
+	    -- _size;
+	    allocator_for_node.deallocate(tmp, 1);
+    }
 
     template <class... Args>
     iterator emplace(const_iterator pos, Args&&... args);
@@ -110,7 +331,17 @@ public:
     template <class... Args>
     void emplace_front(Args&&... args);
 
-    void resize(size_t count);
+    void resize(size_t count) {
+    	if (count > _size) {
+    		while (_size != count)
+    			this->push_back();
+    	}
+    	else if (count < _size) {
+    		while (_size != count)
+    			this->pop_back();
+    	}
+    };
+
     void swap(list& other);
 
 
@@ -123,12 +354,8 @@ public:
 
     // Your code goes here?..
 
-private:
-
-    // Your code goes here...
-
 };
 
-// Your template function definitions may go here...
+
 
 }  // namespace task
--- list/test/test.cpp
+++ list/test/test.cpp
@@ -113,222 +113,222 @@ int main() {
     }
 
 
-    {
-        task::list<Immovable> list(5);
-        task::list<Immovable> list2 = std::move(list);
-        list = std::move(list2);
-        list2 = task::list<Immovable>(10);
-        list.swap(list2);
-    }
+    // {
+        // task::list<Immovable> list(5);
+    //     task::list<Immovable> list2 = std::move(list);
+    //     list = std::move(list2);
+    //     list2 = task::list<Immovable>(10);
+    //     list.swap(list2);
+    // }
 
 
-    {
-        task::list<MoveTester> list(5);
-        list.push_back(MoveTester());
-        list.push_front(MoveTester());
-        list.insert(std::next(list.begin()), MoveTester());
-        ASSERT_TRUE_MSG(list.back().action == "MC", "rvalue push_back")
-        ASSERT_TRUE_MSG(list.back().action == "MC", "rvalue push_front")
-        ASSERT_TRUE_MSG(std::next(list.cbegin())->action == "MC", "rvalue insert")
+    // {
+    //     task::list<MoveTester> list(5);
+    //     list.push_back(MoveTester());
+    //     list.push_front(MoveTester());
+    //     list.insert(std::next(list.begin()), MoveTester());
+    //     ASSERT_TRUE_MSG(list.back().action == "MC", "rvalue push_back")
+    //     ASSERT_TRUE_MSG(list.back().action == "MC", "rvalue push_front")
+    //     ASSERT_TRUE_MSG(std::next(list.cbegin())->action == "MC", "rvalue insert")
 
-        task::list<ArgForwardTester> list2;
-        MoveTester mt;    // reusable after move because object is left valid
-        list2.emplace_back(MoveTester(), mt, std::move(mt));
-        list2.emplace_front(mt, std::move(mt), MoveTester());
-        list2.emplace(std::next(list2.begin()), std::move(mt), MoveTester(), mt);
-        ASSERT_TRUE_MSG(list2.back().actions == "MCCCMC", "emplace_back")
-        ASSERT_TRUE_MSG(list2.front().actions == "CCMCMC", "emplace_front")
-        ASSERT_TRUE_MSG(std::next(list2.begin())->actions == "MCMCCC", "emplace")
-    }
-
-
-    {
-        task::list<size_t> list_task(10, 30);
-        std::list<size_t> list_std(10, 30);
-        ASSERT_EQUAL_MSG(list_task, list_std, "Count-value constructor")
-
-        list_task.insert(list_task.begin(), 20);
-        list_std.insert(list_std.begin(), 20);
-
-        list_task.insert(list_task.end(), 10, 20);
-        list_std.insert(list_std.end(), 10, 20);
-
-        ASSERT_EQUAL_MSG(list_task, list_std, "list::insert")
-
-        list_task.erase(list_task.begin(), std::next(list_task.begin(), 5));
-        list_std.erase(list_std.begin(), std::next(list_std.begin(), 5));
-
-        list_task.erase(std::prev(list_task.end(), 5), list_task.end());
-        list_std.erase(std::prev(list_std.end(), 5), list_std.end());
-
-        ASSERT_EQUAL_MSG(list_task, list_std, "list::erase")
-    }
+    //     task::list<ArgForwardTester> list2;
+    //     MoveTester mt;    // reusable after move because object is left valid
+    //     list2.emplace_back(MoveTester(), mt, std::move(mt));
+    //     list2.emplace_front(mt, std::move(mt), MoveTester());
+    //     list2.emplace(std::next(list2.begin()), std::move(mt), MoveTester(), mt);
+    //     ASSERT_TRUE_MSG(list2.back().actions == "MCCCMC", "emplace_back")
+    //     ASSERT_TRUE_MSG(list2.front().actions == "CCMCMC", "emplace_front")
+    //     ASSERT_TRUE_MSG(std::next(list2.begin())->actions == "MCMCCC", "emplace")
+    // }
 
 
-    {
-        task::list<size_t> list;
-        RandomFill(list, RandomUInt(1000, 5000));
-        list.sort();
-        ASSERT_TRUE(std::is_sorted(list.begin(), list.end()))
+    // {
+    //     task::list<size_t> list_task(10, 30);
+    //     std::list<size_t> list_std(10, 30);
+    //     ASSERT_EQUAL_MSG(list_task, list_std, "Count-value constructor")
 
-        task::list<size_t> list2 = list;
-        ASSERT_EQUAL_MSG(list, list2, "Assignment operator")
+    //     list_task.insert(list_task.begin(), 20);
+    //     list_std.insert(list_std.begin(), 20);
 
-        list2.resize(0);
-        for (auto it = list.crbegin(); it != list.crend(); ++it) {
-            list2.push_back(*it);
-        }
-        list.reverse();
-        ASSERT_EQUAL_MSG(list, list2, "list::reverse / const reverse iterator")
+    //     list_task.insert(list_task.end(), 10, 20);
+    //     list_std.insert(list_std.end(), 10, 20);
 
-        std::reverse(list.begin(), list.end());
-        list2.reverse();
+    //     ASSERT_EQUAL_MSG(list_task, list_std, "list::insert")
 
-        ASSERT_EQUAL_MSG(list, list2, "std::reverse")
-        ASSERT_TRUE(std::is_sorted(list.begin(), list.end()))
+    //     list_task.erase(list_task.begin(), std::next(list_task.begin(), 5));
+    //     list_std.erase(list_std.begin(), std::next(list_std.begin(), 5));
 
-        task::list<size_t> list3(10);
-        list = list2 = list3;
-        ASSERT_EQUAL_MSG(list, list3, "Assignment operator")
-    }
+    //     list_task.erase(std::prev(list_task.end(), 5), list_task.end());
+    //     list_std.erase(std::prev(list_std.end(), 5), list_std.end());
+
+    //     ASSERT_EQUAL_MSG(list_task, list_std, "list::erase")
+    // }
 
 
-    {
-        task::list<size_t> list_task;
-        std::list<size_t> list_std;
+    // {
+    //     task::list<size_t> list;
+    //     RandomFill(list, RandomUInt(1000, 5000));
+    //     list.sort();
+    //     ASSERT_TRUE(std::is_sorted(list.begin(), list.end()))
 
-        RandomFill(list_std, RandomUInt(1000, 5000), 100);
+    //     task::list<size_t> list2 = list;
+    //     ASSERT_EQUAL_MSG(list, list2, "Assignment operator")
 
-        list_task.resize(list_std.size());
-        std::copy(list_std.begin(), list_std.end(), list_task.begin());
+    //     list2.resize(0);
+    //     for (auto it = list.crbegin(); it != list.crend(); ++it) {
+    //         list2.push_back(*it);
+    //     }
+    //     list.reverse();
+    //     ASSERT_EQUAL_MSG(list, list2, "list::reverse / const reverse iterator")
 
-        ASSERT_EQUAL_MSG(list_task, list_std, "std::copy")
+    //     std::reverse(list.begin(), list.end());
+    //     list2.reverse();
 
-        list_task.sort();
-        list_std.sort();
+    //     ASSERT_EQUAL_MSG(list, list2, "std::reverse")
+    //     ASSERT_TRUE(std::is_sorted(list.begin(), list.end()))
 
-        ASSERT_EQUAL_MSG(list_task, list_std, "list::sort")
+    //     task::list<size_t> list3(10);
+    //     list = list2 = list3;
+    //     ASSERT_EQUAL_MSG(list, list3, "Assignment operator")
+    // }
 
-        list_task.unique();
-        list_std.unique();
 
-        ASSERT_EQUAL_MSG(list_task, list_std, "list::unique")
+    // {
+    //     task::list<size_t> list_task;
+    //     std::list<size_t> list_std;
 
-        task::list<size_t> list_task2;
-        std::list<size_t> list_std2;
+    //     RandomFill(list_std, RandomUInt(1000, 5000), 100);
 
-        RandomFill(list_std2, RandomUInt(100, 500), 100);
+    //     list_task.resize(list_std.size());
+    //     std::copy(list_std.begin(), list_std.end(), list_task.begin());
 
-        list_task2.resize(list_std2.size());
-        std::copy(list_std2.begin(), list_std2.end(), list_task2.begin());
+    //     ASSERT_EQUAL_MSG(list_task, list_std, "std::copy")
 
-        {
-            auto list_task_temp = list_task2;
-            auto list_std_temp = list_std2;
+    //     list_task.sort();
+    //     list_std.sort();
 
-            auto& element_reference = list_task2.front();
+    //     ASSERT_EQUAL_MSG(list_task, list_std, "list::sort")
 
-            list_task.splice(++list_task.begin(), list_task2);
-            list_std.splice(++list_std.begin(), list_std2);
+    //     list_task.unique();
+    //     list_std.unique();
 
-            element_reference = 101;
-            *++list_std.begin() = 101;
+    //     ASSERT_EQUAL_MSG(list_task, list_std, "list::unique")
 
-            ASSERT_EQUAL_MSG(list_task, list_std, "list::splice")
+    //     task::list<size_t> list_task2;
+    //     std::list<size_t> list_std2;
 
-            list_task2 = std::move(list_task_temp);
-            list_std2 = std::move(list_std_temp);
+    //     RandomFill(list_std2, RandomUInt(100, 500), 100);
 
-            ASSERT_EQUAL_MSG(list_task2, list_std2, "move operator=")
-        }
+    //     list_task2.resize(list_std2.size());
+    //     std::copy(list_std2.begin(), list_std2.end(), list_task2.begin());
 
-        list_task.remove(list_task.front());
-        list_std.remove(list_std.front());
+    //     {
+    //         auto list_task_temp = list_task2;
+    //         auto list_std_temp = list_std2;
 
-        ASSERT_EQUAL_MSG(list_task, list_std, "list::remove")
+    //         auto& element_reference = list_task2.front();
 
-        list_task.sort();
-        list_std.sort();
-        list_task2.sort();
-        list_std2.sort();
+    //         list_task.splice(++list_task.begin(), list_task2);
+    //         list_std.splice(++list_std.begin(), list_std2);
 
-        list_task.swap(list_task2);
-        list_std.swap(list_std2);
+    //         element_reference = 101;
+    //         *++list_std.begin() = 101;
 
-        ASSERT_EQUAL_MSG(list_task2, list_std2, "list::swap")
+    //         ASSERT_EQUAL_MSG(list_task, list_std, "list::splice")
 
-        {
-            auto &element_reference = list_task2.front();
+    //         list_task2 = std::move(list_task_temp);
+    //         list_std2 = std::move(list_std_temp);
 
-            list_task.merge(list_task2);
-            list_std.merge(list_std2);
+    //         ASSERT_EQUAL_MSG(list_task2, list_std2, "move operator=")
+    //     }
 
-            ASSERT_EQUAL_MSG(list_task, list_std, "list::merge")
-            ASSERT_EQUAL_MSG(list_task2, list_std2, "list::merge")
+    //     list_task.remove(list_task.front());
+    //     list_std.remove(list_std.front());
 
-            element_reference = 1000;
+    //     ASSERT_EQUAL_MSG(list_task, list_std, "list::remove")
 
-            ASSERT_TRUE_MSG(std::find(list_task.begin(), list_task.end(), 1000) != list_task.end(), "list::merge")
-        }
-    }
+    //     list_task.sort();
+    //     list_std.sort();
+    //     list_task2.sort();
+    //     list_std2.sort();
 
-    {
-        const size_t LIST_COUNT = 5;
-        const size_t ITER_COUNT = 4000;
-        const size_t MAX_SIZE = 1000;
+    //     list_task.swap(list_task2);
+    //     list_std.swap(list_std2);
 
-        std::vector<task::list<size_t>> lists_task(LIST_COUNT);
-        std::vector<std::list<size_t>> lists_std(LIST_COUNT);
+    //     ASSERT_EQUAL_MSG(list_task2, list_std2, "list::swap")
 
-        for (size_t iter = 0; iter < ITER_COUNT; ++iter) {
-            for (size_t list = 0; list < LIST_COUNT; ++list) {
-                if (TossCoin()) {
-                    // Random Push
-                    size_t count = RandomUInt(0, MAX_SIZE - lists_task[list].size());
-                    for (; count; --count) {
-                        auto val = RandomUInt();
-                        if (TossCoin()) {
-                            lists_task[list].push_back(val);
-                            lists_std[list].push_back(val);
-                        } else {
-                            lists_task[list].push_front(val);
-                            lists_std[list].push_front(val);
-                        }
-                    }
-                } else {
-                    // Random Pop
-                    size_t count = RandomUInt(0, lists_task[list].size());
-                    for (; count; --count) {
-                        if (TossCoin()) {
-                            lists_task[list].pop_back();
-                            lists_std[list].pop_back();
-                        } else {
-                            lists_task[list].pop_front();
-                            lists_std[list].pop_front();
-                        }
-                    }
-                }
+    //     {
+    //         auto &element_reference = list_task2.front();
 
-                switch (RandomUInt(4)) {
-                    case 0:
-                        if (!lists_std[list].empty()) {
-                            lists_task[list].remove(lists_task[list].back());
-                            lists_std[list].remove(lists_std[list].back());
-                        }
-                        break;
-                    case 1:
-                        lists_task[list].reverse();
-                        lists_std[list].reverse();
-                        break;
-                    case 2:
-                        lists_task[list].sort();
-                        lists_std[list].sort();
-                        break;
-                }
+    //         list_task.merge(list_task2);
+    //         list_std.merge(list_std2);
 
-                ASSERT_EQUAL_MSG(lists_task[list], lists_std[list], "Stress test")
-            }
-        }
-    }
+    //         ASSERT_EQUAL_MSG(list_task, list_std, "list::merge")
+    //         ASSERT_EQUAL_MSG(list_task2, list_std2, "list::merge")
+
+    //         element_reference = 1000;
+
+    //         ASSERT_TRUE_MSG(std::find(list_task.begin(), list_task.end(), 1000) != list_task.end(), "list::merge")
+    //     }
+    // }
+
+    // {
+    //     const size_t LIST_COUNT = 5;
+    //     const size_t ITER_COUNT = 4000;
+    //     const size_t MAX_SIZE = 1000;
+
+    //     std::vector<task::list<size_t>> lists_task(LIST_COUNT);
+    //     std::vector<std::list<size_t>> lists_std(LIST_COUNT);
+
+    //     for (size_t iter = 0; iter < ITER_COUNT; ++iter) {
+    //         for (size_t list = 0; list < LIST_COUNT; ++list) {
+    //             if (TossCoin()) {
+    //                 // Random Push
+    //                 size_t count = RandomUInt(0, MAX_SIZE - lists_task[list].size());
+    //                 for (; count; --count) {
+    //                     auto val = RandomUInt();
+    //                     if (TossCoin()) {
+    //                         lists_task[list].push_back(val);
+    //                         lists_std[list].push_back(val);
+    //                     } else {
+    //                         lists_task[list].push_front(val);
+    //                         lists_std[list].push_front(val);
+    //                     }
+    //                 }
+    //             } else {
+    //                 // Random Pop
+    //                 size_t count = RandomUInt(0, lists_task[list].size());
+    //                 for (; count; --count) {
+    //                     if (TossCoin()) {
+    //                         lists_task[list].pop_back();
+    //                         lists_std[list].pop_back();
+    //                     } else {
+    //                         lists_task[list].pop_front();
+    //                         lists_std[list].pop_front();
+    //                     }
+    //                 }
+    //             }
+
+    //             switch (RandomUInt(4)) {
+    //                 case 0:
+    //                     if (!lists_std[list].empty()) {
+    //                         lists_task[list].remove(lists_task[list].back());
+    //                         lists_std[list].remove(lists_std[list].back());
+    //                     }
+    //                     break;
+    //                 case 1:
+    //                     lists_task[list].reverse();
+    //                     lists_std[list].reverse();
+    //                     break;
+    //                 case 2:
+    //                     lists_task[list].sort();
+    //                     lists_std[list].sort();
+    //                     break;
+    //             }
+
+    //             ASSERT_EQUAL_MSG(lists_task[list], lists_std[list], "Stress test")
+    //         }
+    //     }
+    // }
 
 }
